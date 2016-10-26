4160115a418d44ba92787fa4f7e56efaf4f85b81
elizarv <eramirezvillamizar@gmail.com>
2016-10-25 22:20:44 -0500

    metodo que retorna los datos de un producto dado su id

--- app/model/productoDAO.php
+++ app/model/productoDAO.php
@@ -6,7 +6,9 @@
 
         public function getProductos() {
             $this->connect();
-            $result = $this->query("SELECT p.id, p.nombre, d.suma FROM producto p, (SELECT SUM( t.total ) AS suma, t.idproducto FROM detalleventa t GROUP BY t.idproducto)d WHERE d.idproducto = p.id ORDER BY d.suma DESC");
+            $result = $this->query("SELECT p.id, p.nombre, d.suma FROM producto p, 
+                                    (SELECT SUM( t.total ) AS suma, t.idproducto FROM detalleventa t GROUP BY t.idproducto)d 
+                                        WHERE d.idproducto = p.id ORDER BY d.suma DESC");
             $this->terminate();
             $arreglo = array();
             while($row = mysqli_fetch_array($result)) {
@@ -25,20 +27,29 @@
             }
         }
 
-        public setVenta($valortotal, $valordescuentos, $valoriva, $idcliente){
+        public function setVenta($valortotal, $valordescuentos, $valoriva, $idcliente){
             $this->connect();
             $restult = $this->query("INSERT INTO venta (numerofactura, valortotal, valordescuentos, valoriva, idcliente) 
                                         values (".$this->serial.",".$valortotal.",".$valordescuentos.",".$valoriva.",".$idcliente.")");
             $this->terminate();
         }
 
+        public function getProductoPorId($id){
+            $this->connect();
+            $result = $this->query("SELECT p.existencias, p.nombre, p.tipo, p.precioventa FROM producto p WHERE p.id=".$id.);
+            $this->terminate();
+            $row = mysqli_fetch($result);
+            $DTO = new ProductoDTO(null, $row['nombre'], $row['tipo'], $row['precioventa'], $row['existencias'], null);
+            return $DTO;
+        }
+
         public function getTotalProductos() {
             $this->connect();
-            $result = $this->query("SELECT id, nombre, existencias, precioVenta, tipo FROM producto");
+            $result = $this->query("SELECT id, nombre, existencias, precioventa, tipo FROM producto");
             $this->terminate();
             $arreglo = array();
             while($row = mysqli_fetch_array($result)) {
-                $DTO = new ProductoDTO($row['id'], $row['nombre'], $row['tipo'], $row['precioVenta'], $row['existencias'], null);
+                $DTO = new ProductoDTO($row['id'], $row['nombre'], $row['tipo'], $row['precioventa'], $row['existencias'], null);
                 array_push($arreglo, $DTO);
             }
             return $arreglo;
