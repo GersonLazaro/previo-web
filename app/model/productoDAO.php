ab3de18607da5b0a5dfcb18a65630da8b7b315f1
elizarv <eramirezvillamizar@gmail.com>
2016-10-25 22:04:25 -0500

    agregue el terminate de la coneccion y el metodo de agregar venta

--- app/model/productoDAO.php
+++ app/model/productoDAO.php
@@ -2,11 +2,12 @@
     include_once 'app/model/model.php';
 
     class ProductoDAO extends Model {
-        
+        private $serial;
 
         public function getProductos() {
             $this->connect();
             $result = $this->query("SELECT p.id, p.nombre, d.suma FROM producto p, (SELECT SUM( t.total ) AS suma, t.idproducto FROM detalleventa t GROUP BY t.idproducto)d WHERE d.idproducto = p.id ORDER BY d.suma DESC");
+            $this->terminate();
             $arreglo = array();
             while($row = mysqli_fetch_array($result)) {
                 $DTO = new ProductoDTO($row['id'], $row['nombre'], null, null, null, $row['suma']);
@@ -15,9 +16,26 @@
             return $arreglo;
         }
 
+        public function getSerial(){
+            $this->connect();
+            $result = $this->query("SELECT numerofactura FROM venta ORDER BY numerofactura DESC LIMIT 1");
+            $this->terminate();
+            if($row = mysqli_fetch($result)){
+                $serial = $row['numerofactura']+1;
+            }
+        }
+
+        public setVenta($valortotal, $valordescuentos, $valoriva, $idcliente){
+            $this->connect();
+            $restult = $this->query("INSERT INTO venta (numerofactura, valortotal, valordescuentos, valoriva, idcliente) 
+                                        values (".$this->serial.",".$valortotal.",".$valordescuentos.",".$valoriva.",".$idcliente.")");
+            $this->terminate();
+        }
+
         public function getTotalProductos() {
             $this->connect();
             $result = $this->query("SELECT id, nombre, existencias, precioVenta, tipo FROM producto");
+            $this->terminate();
             $arreglo = array();
             while($row = mysqli_fetch_array($result)) {
                 $DTO = new ProductoDTO($row['id'], $row['nombre'], $row['tipo'], $row['precioVenta'], $row['existencias'], null);
