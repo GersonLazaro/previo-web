2ce7a7edc85ffd31a9370ce5eb8e70e7b0640e3c
elizarv <eramirezvillamizar@gmail.com>
2016-10-26 02:41:04 -0500

    terminado y probado

--- app/model/ventaDAO.php
+++ app/model/ventaDAO.php
@@ -6,8 +6,10 @@
        
         public function setDetalleventa($idproducto, $cantidad, $valoriva, $valordescuento, $total){
             $this->connect();
+            echo $this->serial;
+            echo "aca";
             $this->query("INSERT INTO detalleventa (idproducto, idfactura, cantidad, valoriva, valordescuento, total) 
-                             values ('".$idproducto."','".$this->serial."','".$cantidad."','".$valoriva."','".$valordescuento."','".$total."')");
+                             values ('".$idproducto."',".$this->serial.",'".$cantidad."','".$valoriva."','".$valordescuento."','".$total."')");
             $result= $this->query("SELECT p.existencias FROM producto p WHERE p.id=".$idproducto);
             $nuevaCantidad = mysqli_fetch_array($result)['existencias']- $cantidad;
             $this->query("UPDATE producto SET existencias=".$nuevaCantidad);
@@ -21,20 +23,13 @@
             }
         }
 
-        public function getSerial(){
-            $this->connect();
-            $result = $this->query("SELECT numerofactura FROM venta ORDER BY numerofactura DESC LIMIT 1");
-            $this->terminate();
-            if($row = mysqli_fetch_array($result)){
-                $this->serial = $row['numerofactura']+1;
-            }
-        }
-
         public function setVenta($valortotal, $valordescuentos, $valoriva, $idcliente){
-            $this->getSerial();
             $this->connect();
-            $this->query("INSERT INTO venta (numerofactura, valortotal, valordescuentos, valoriva, idcliente) 
-                                 values ('".$this->serial."','".$valortotal."','".$valordescuentos."','".$valoriva."','".$idcliente."')");
+            $this->query("INSERT INTO venta (valortotal, valordescuentos, valoriva, idcliente) 
+                                 values ('".$valortotal."','".$valordescuentos."','".$valoriva."','".$idcliente."')");
+            $consulta="SELECT * FROM venta order by numerofactura desc LIMIT 1";
+            $row=mysqli_fetch_array($this->query($consulta));
+            $this->serial = $row['numerofactura'];
             $this->terminate();
         }
 
